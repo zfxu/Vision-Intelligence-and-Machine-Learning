2b717f83fc4b7959e97058cdc59fd429e759fb08
Adam <awoox2@gmail.com>
2018-01-12 22:07:11 -0500

    Week 1

---
+++ Week_1/Assignement2.m
@@ -0,0 +1,7 @@
+d1_ref = 4;
+d2_ref = 20;
+H1 = 4;
+H2 = 6;
+ratio = 2;
+f_ref = 400;
+[ f, pos ] = compute_f_pos( d1_ref, d2_ref, H1, H2, ratio, f_ref )
---
+++ Week_1/Assignment1.m
@@ -0,0 +1,4 @@
+d_ref = 4;
+f_ref = 400;
+pos = -5;
+f = compute_focal_length( d_ref, f_ref, pos )
